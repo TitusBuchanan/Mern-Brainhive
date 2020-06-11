448cb5e5b9d40581359ac77ae316006bf8f20e59
titus buchanan <titusbuchananjr@gmail.com>
2020-06-11 14:59:28 -0400

    connected to database

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const config = require('./config/default.json');
+const mongoose = require('mongoose')
 
 app.use(cors());
+
+app.use(express.json());
 const PORT = process.env.PORT || 5000
 
-
+mongoose.connect(
+    config.mongoURI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    },
+    () => console.log("connected to database")
+  );
+  
 
 
 app.listen(PORT, () => console.log('Server Running'))
