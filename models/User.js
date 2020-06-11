816ffe4b9465e3fb8c2e7073134b7970a0580a7d
titus buchanan <titusbuchananjr@gmail.com>
2020-06-11 17:48:15 -0400

    added model and route

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength:3,
+      maxlength:30
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: Date,
+  },
+  { timestamps: {} }
+);
+
+module.exports = User = mongoose.model("users", userSchema);
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000
 
+const users = require('./routes/api/users');
+
+app.use("/api/users", users);
+
 mongoose.connect(
     config.mongoURI,
     {
