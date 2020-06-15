832b8e3c8224d82d824e69b244e008fd080774c4
titus buchanan <titusbuchananjr@gmail.com>
2020-06-14 22:22:09 -0400

    added validation to our User schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,49 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
+const {check, validationResult} = require('express-validator');
+const User = require('../../models/User');
 const router = express.Router();
 
 // @route		POST api/users
 // @desc		create new user
 // @access	public
-router.post("/", (req, res) => {
-  res.json(req.body);
-});
+router.post("/", 
+[
+    check('email','Email Required').not().isEmpty(),
+    check('email','Valid Email Required').isEmail(),
+    check('password','Please enter a password with at least six chrachters').isLength({ min:6 })
+],
+ async (req, res) => {
+    const errors= validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+  
+  
+    const userData = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+    userData.password = await bcrypt.hash(userData.password, salt)
+    
+    const user = await User.create(userData)
+
+    res.json(user);
+    } catch(error) {
+        console.error(error);
+        res.status(500).json(error);
+    }
+    
+    
+
+  
+ }
+);
+
+
 
 module.exports = router;
