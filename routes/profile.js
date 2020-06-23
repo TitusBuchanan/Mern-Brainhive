c705a8c4e002cdb809c2a00fa2abc709f0b669bc
titus buchanan <titusbuchananjr@gmail.com>
2020-06-23 17:13:58 -0400

    updated profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const config = require('config');
+// create the post (register profile) route. for now make it a public url and expect userid in the body.
+const express = require("express");
+const config = require("config");
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const auth = require("../../middleware/auth");
+
+const { check, validationResult } = require("express-validator/check");
+
+const Profile = require("../../models/Profile");
+
+router.get("/", auth, (req, res) => {
+  console.log(req.user);
+  res.send("Profile Route");
+});
+
 //@route  POST api/profile
 //@desc   Create or update user profile
 //@access Private
 router.post(
-  '/',
+  "/",
+  auth,
   [
-    check('firstName', 'first name is required').not().isEmpty(),
-    check('lastName', 'Last name is required').not().isEmpty(),
-    check('name', 'Name is required').not().isEmpty(),
-    check('educationLevel', 'Education level is required').not().isEmpty(),
+    check("firstName", "first name is required").not().isEmpty(),
+    check("lastName", "Last name is required").not().isEmpty(),
+    check("educationLevel", "Education level is required").not().isEmpty(),
   ],
   async (req, res) => {
     console.log(req.body);
@@ -25,37 +34,45 @@ router.post(
       const {
         firstName,
         lastName,
-        name,
         occupation,
         educationLevel,
-        resourceId,
         certifications,
         location,
         social,
+        summary,
       } = req.body;
+
       const userId = req.user.id;
+
       const profileFields = {
         firstName,
         lastName,
-        name,
+        name: `${firstName} ${lastName}`,
+        educationLevel,
       };
       profileFields.user = userId;
       if (occupation) profileFields.occupation = occupation;
-      if (educationLevel) profileFields.educationLevel = educationLevel;
-      if (resourceId) profileFields.resourceId = resourceId;
       if (certifications) profileFields.certifications = certifications;
       if (location) profileFields.location = location;
+      if (summary) profileFields.summary = summary;
+
       profileFields.social = {};
-      if (githubUrl) profileFields.social.githubUrl = githubUrl;
-      if (twitterUrl) profileFields.social.twitterUrl = twitterUrl;
-      if (youtubeUrl) profileFields.social.youtubeUrl = youtubeUrl;
-      profile = new Profile(profileFields);
-      await profile.save();
+      if (social.githubUrl) profileFields.social.githubUrl = social.githubUrl;
+      if (social.twitterUrl)
+        profileFields.social.twitterUrl = social.twitterUrl;
+      if (social.youtubeUrl)
+        profileFields.social.youtubeUrl = social.youtubeUrl;
+
+      const profile = await Profile.create(profileFields);
       res.json(profile);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
   }
 );
+
+// get /self return logged in users profile data. authenticated route
+// get / return all profiles - hacker challenge one -> exclude logged in user from results. Hint: query hacker challenge 2 -> exclued location data. Hint: Projections
+
 module.exports = router;
